e09278081f7fd9080ecdc3d352dca198855d4c7a
Spiff <78507772+salowise@users.noreply.github.com>
2025-01-28 16:51:07 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+// class person{
+//     constructor(firstName, lastName, dob){
+//         this.firstName = firstName;
+//         this.lastName = lastName;
+//         this.dob = new Date(dob);
+//         this.photo
+//         this.lastLogin
+//     }
+//     getBirthYear(){
+//         return this.dob.getFullYear();
+//     }
+//     getFullName(){
+//         return '${this.firstName} ${this.lastName}';
+//     }
+// }
+// const person1 = new person ('john', 'philip', '2-3-1990');
+// const person2 = new person ('spiff', 'saloka', '02-21-1998');
+// console.log(person2);
+
+let foodItems = [
+    {
+        Bread : 2000,
+        milkyCreamer : 3000,
+        sugar : 150,
+        milo: 300
+
+    },
+
+    {
+        garri : 1000,
+        soup : 1200,
+        water : 300,
+        meat : 400
+    },
+
+    {
+        spaghetti : 1400,
+        seasoning : 200,
+        tomatoPaste : 500,
+        fish : 1000,
+        vegetableOil : 500,
+        spicies : 100,
+        flavour : 400
+    }
+];
+console.log(foodItems[1]);
